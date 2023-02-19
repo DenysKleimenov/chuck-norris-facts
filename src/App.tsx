442d20176916b9f438ac482a99a50fa69577750a
Denys Kleimenov <denyskleimenovit@gmail.com>
2023-02-19 12:39:40 +0200

    App component was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
-import { Greeting } from './components/Greeting';
+import { useCallback, useState } from 'react';
+import { CategoriesList } from './components/CategoriesList';
+import { Header } from './components/Header';
+import { Joke } from './components/Joke';
+import './styles/app.css';
 
-const App = () => <Greeting />;
+const App: React.FC = () => {
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  const handleClick = useCallback((category: string) => {
+    setSelectedCategory(category);
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <main className="main">
+        <CategoriesList
+          handleClick={handleClick}
+          selectedCategory={selectedCategory}
+        />
+        <Joke selectedCategory={selectedCategory} />
+      </main>
+    </>
+  );
+};
 
 export default App;
